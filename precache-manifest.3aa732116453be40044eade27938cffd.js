13d2b49ba3e5ae2243fc7e947731d7be095a2c08
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 00:45:22 +0300

    Updates

---
+++ precache-manifest.3aa732116453be40044eade27938cffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f959ec7d66b0165e3ffdbb26f24827a",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "af1554c4f14aabfe057b",
+    "url": "/drum-machine/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f95988f7dc60033e9467",
+    "url": "/drum-machine/static/css/main.ffd8ecbe.chunk.css"
+  },
+  {
+    "revision": "af1554c4f14aabfe057b",
+    "url": "/drum-machine/static/js/2.624f6fec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.624f6fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f95988f7dc60033e9467",
+    "url": "/drum-machine/static/js/main.a8306b41.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
